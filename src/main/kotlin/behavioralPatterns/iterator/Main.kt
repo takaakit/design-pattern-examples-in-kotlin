bacba10597936036f3abb9f81726b0f56adcc0e8
takaakit <teshima.takaaki.pc@gmail.com>
2018-07-14 20:56:05 +0900

    Delete unnecessary space

--- src/main/kotlin/behavioralPatterns/iterator/Main.kt
+++ src/main/kotlin/behavioralPatterns/iterator/Main.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     bookShelf.add(Book("The Object Primer: Agile Model-Driven Development with UML 2.0"))
     bookShelf.add(Book("Software Systems Architecture: Working With Stakeholders Using Viewpoints and Perspectives"))
     bookShelf.add(Book("A Practical Guide to SysML: The Systems Modeling Language"))
-    bookShelf.add(Book("A Pattern Language: Towns, Buildings, Construction "))
+    bookShelf.add(Book("A Pattern Language: Towns, Buildings, Construction"))
 
     val it = bookShelf.iterator()
     while (it.hasNext()) {
