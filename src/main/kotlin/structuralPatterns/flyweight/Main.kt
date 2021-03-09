be101d61fa7c9f892a33647295383d9ff064bcd1
takaakit <teshima.takaaki.gh@gmail.com>
2021-03-09 11:22:48 +0900

    Modified Flyweight example

--- src/main/kotlin/structuralPatterns/flyweight/Main.kt
+++ src/main/kotlin/structuralPatterns/flyweight/Main.kt
@@ -3,7 +3,7 @@ package structuralPatterns.flyweight
 import java.util.*
 
 /*
-Display a string consisting of large characters (numbers and hyphen only). Large character objects are not created until they are needed. And the created objects are reused.
+Display a string consisting of large characters (0-9 digits only). Large character objects are not created until they are needed. And the created objects are reused.
 
 Example Output
 -----
