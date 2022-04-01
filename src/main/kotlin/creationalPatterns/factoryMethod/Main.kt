dc05fe6a1a7b65ac808895012bae9cccf119d060
takaakit <takaaki.teshima.dev@gmail.com>
2022-04-02 07:49:39 +0900

    Fixed typo

--- src/main/kotlin/creationalPatterns/factoryMethod/Main.kt
+++ src/main/kotlin/creationalPatterns/factoryMethod/Main.kt
@@ -3,7 +3,7 @@ package creationalPatterns.factoryMethod
 import creationalPatterns.factoryMethod.creditCard.CreditCardFactory
 
 /*
-The subject is a factory to make credit cards. The Factory defines how to create an credit card,
+The subject is a factory to make credit cards. The Factory defines how to create a credit card,
 but the actual credit card is created by the CreditCardFactory.
 The "createProduct()" is called a Factory Method, and it is responsible for manufacturing an object.
  */
