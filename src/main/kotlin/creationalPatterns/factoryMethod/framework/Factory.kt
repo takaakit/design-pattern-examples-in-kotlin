6a9fffb17896d0a76d805119d046b9d51726c885
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-23 18:10:05 +0900

    Edit comments

--- src/main/kotlin/creationalPatterns/factoryMethod/framework/Factory.kt
+++ src/main/kotlin/creationalPatterns/factoryMethod/framework/Factory.kt
@@ -10,11 +10,12 @@ abstract class Factory {
 
     fun create(owner: String): Product {
         // ˅
-        // Write pre-creation code here, if any.
+        // Write pre-creation code here.
 
+        // Encapsulate the knowledge of which Product subclass to create and move this knowledge out of the framework.
         val product = createProduct(owner)
 
-        // Write post-creation code here, if any.
+        // Write post-creation code here.
 
         return product
         // ˄
--- src/main/kotlin/creationalPatterns/prototype/framework/Manager.kt
+++ src/main/kotlin/creationalPatterns/prototype/framework/Manager.kt
@@ -20,7 +20,7 @@ class Manager {
     fun getDisplay(displayName: String): Display {
         // ˅
         val d = display[displayName]
-        return d!!.clone()
+        return d!!.clone()      // Create a new object by asking a concrete class to clone itself. Therefore, do not need to know the concrete Display class name.
         // ˄
     }
 
