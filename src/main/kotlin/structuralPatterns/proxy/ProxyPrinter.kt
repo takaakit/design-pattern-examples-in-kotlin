d323ebe3a98c569648af3693e755eb0e74bb197f
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-30 15:50:27 +0900

    Add comments

--- src/main/kotlin/structuralPatterns/proxy/ProxyPrinter.kt
+++ src/main/kotlin/structuralPatterns/proxy/ProxyPrinter.kt
@@ -2,6 +2,7 @@ package structuralPatterns.proxy
 // ˅
 // ˄
 
+// ProxyPrinter forwards requests to RealPrinter when appropriate.
 class ProxyPrinter(name: String) : Printer {
     // ˅
     
@@ -33,15 +34,18 @@ class ProxyPrinter(name: String) : Printer {
         if (real != null) {
             real!!.changeName(name)
         }
+        
         currentName = name
         // ˄
     }
 
     override fun output(content: String) {
         // ˅
+        // Check to see if the the RealPrinter had been created, create it if necessary.
         if (real == null) {
             real = RealPrinter(currentName)
         }
+        
         real!!.output(content)
         // ˄
     }
